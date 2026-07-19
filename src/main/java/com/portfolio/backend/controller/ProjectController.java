8b7144f1219a16e3311ac72ea219ea35842172ae
Sanju@123456 <sanjunadar2004@gmail.com>
2026-07-19 19:50:19 +0530

    update cors for production

--- src/main/java/com/portfolio/backend/controller/ProjectController.java
+++ src/main/java/com/portfolio/backend/controller/ProjectController.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
-@CrossOrigin(origins = "http://localhost:3000") // React dev server ku permission
+//@CrossOrigin(origins = "http://localhost:3000") // React dev server ku permission
+@CrossOrigin(origins = "*")
 public class ProjectController {
 
     private final ProjectService projectService;
